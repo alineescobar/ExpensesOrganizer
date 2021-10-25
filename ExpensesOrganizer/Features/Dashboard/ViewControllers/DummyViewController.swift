242a65e0c9cbdcb3831aa83e5c033159d2b6ec58
Aline Escobar <21069293+alineescobar@users.noreply.github.com>
2021-10-25 17:50:01 -0300

    creating xib for the transactions and testing it on a dummy storyboard to avoid conflicts xD

---
+++ ExpensesOrganizer/Features/Dashboard/ViewControllers/DummyViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DummyViewController.swift
+//  ExpensesOrganizer
+//
+//  Created by Aline Osana Escobar on 25/10/21.
+//
+
+import UIKit
+
+class DummyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableViewDummy: UITableView!
+    let customCellId = "TransactionCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableViewDummy.register(UINib.init(nibName: customCellId, bundle: nil), forCellReuseIdentifier: customCellId)
+        tableViewDummy.reloadData()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableViewDummy.dequeueReusableCell(withIdentifier: customCellId, for: indexPath) as? TransactionCell else {
+            return UITableViewCell()
+        }
+        
+        cell.transactionName.text = "Netflix"
+        cell.transactionTag.text = "Assinatura"
+        cell.transactionDate.text = "20 out"
+        cell.transactionPrice.text = "-9,50"
+        
+        return cell
+    }
+    
+}
