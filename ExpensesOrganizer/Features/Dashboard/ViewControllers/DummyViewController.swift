900a211c8d44ef0a559fb03bbe74551ab21924e4
Aline Escobar <21069293+alineescobar@users.noreply.github.com>
2021-10-26 16:27:35 -0300

    creating xib for the header

--- ExpensesOrganizer/Features/Dashboard/ViewControllers/DummyViewController.swift
+++ ExpensesOrganizer/Features/Dashboard/ViewControllers/DummyViewController.swift
@@ -7,33 +7,61 @@
 
 import UIKit
 
-class DummyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+enum TransactionCells {
+    case header, cell, emptyState
+    
+    static var allCases: [TransactionCells] {
+        return [.header, .cell, .emptyState]
+    }
+}
 
+class DummyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var tableViewDummy: UITableView!
     let customCellId = "TransactionCell"
+    let customCellHeader = "TransactionsHeaderCell"
+    let transactionsID = "transactions"
+    private let transactionCells: [TransactionCells] = TransactionCells.allCases
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableViewDummy.register(UINib.init(nibName: customCellId, bundle: nil), forCellReuseIdentifier: customCellId)
+        tableViewDummy.register(UINib.init(nibName: customCellHeader, bundle: nil), forCellReuseIdentifier: customCellHeader)
         tableViewDummy.reloadData()
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableViewDummy.dequeueReusableCell(withIdentifier: customCellId, for: indexPath) as? TransactionCell else {
+        let transactionCells = transactionCells[indexPath.row]
+        
+        switch transactionCells {
+        case .header:
+            guard let cell = tableViewDummy.dequeueReusableCell(withIdentifier: customCellHeader, for: indexPath) as? TransactionsHeaderCell else {
+                return UITableViewCell()
+            }
+            cell.selectionStyle = .none
+            return cell
+            
+        case .cell:
+            guard let cell = tableViewDummy.dequeueReusableCell(withIdentifier: customCellId, for: indexPath) as? TransactionCell else {
+                return UITableViewCell()
+            }
+
+            cell.transactionImage.image = UIImage(named: "subscriptions")
+            cell.transactionName.text = "Netflix"
+            cell.transactionTag.text = "Assinatura"
+            cell.transactionDate.text = "20 out"
+            cell.transactionPrice.text = "-9,50"
+            cell.selectionStyle = .none
+            
+            return cell
+        default:
             return UITableViewCell()
         }
-        
-        cell.transactionName.text = "Netflix"
-        cell.transactionTag.text = "Assinatura"
-        cell.transactionDate.text = "20 out"
-        cell.transactionPrice.text = "-9,50"
-        
-        return cell
     }
     
 }
