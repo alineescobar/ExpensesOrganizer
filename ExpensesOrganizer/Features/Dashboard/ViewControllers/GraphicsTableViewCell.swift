8f3471b7ea5880306691e94d96f8392946c825b5
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-10-28 15:31:23 -0300

    Adjusting chart layout (rm points, legent and changing font)

--- ExpensesOrganizer/Features/Dashboard/ViewControllers/GraphicsTableViewCell.swift
+++ ExpensesOrganizer/Features/Dashboard/ViewControllers/GraphicsTableViewCell.swift
@@ -9,7 +9,7 @@ import Charts
 import UIKit
 
 class GraphicsTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var chartView: LineChartView!
     
     var yValues: [ChartDataEntry] = []
@@ -18,22 +18,24 @@ class GraphicsTableViewCell: UITableViewCell {
         super.awakeFromNib()
         
         chartView.backgroundColor = .systemGray5
-        chartView.rightAxis.enabled = false
         chartView.gridBackgroundColor = .systemGray5
-        
-//        chartView.xAxis.drawGridLinesEnabled = false
-//        chartView.xAxis.drawAxisLineEnabled = false
-//        chartView.drawGridBackgroundEnabled = false
-//        chartView.leftAxis.drawAxisLineEnabled = false
-//
+        chartView.rightAxis.enabled = false
         chartView.leftAxis.enabled = false
         chartView.rightAxis.enabled = false
-        chartView.xAxis.enabled = false
+        chartView.legend.enabled = false
+        chartView.xAxis.labelPosition = .bottom
         chartView.xAxis.gridColor = .clear
         chartView.leftAxis.gridColor = .clear
         chartView.rightAxis.gridColor = .clear
+        chartView.xAxis.labelFont = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        chartView.xAxis.avoidFirstLastClippingEnabled = true
         
-        for i in 0..<20 {
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        chartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: months)
+        chartView.xAxis.granularity = 1
+        
+        // Mock
+        for i in 6..<12 {
             yValues += [ChartDataEntry(x: Double(i), y: Double.random(in: 0..<50))]
         }
         
@@ -44,19 +46,14 @@ class GraphicsTableViewCell: UITableViewCell {
         let set1 = LineChartDataSet(entries: yValues, label: "Rendimento")
         set1.mode = .cubicBezier
         set1.lineWidth = 3
+        set1.drawCirclesEnabled = false
         set1.setColor(.darkGray)
         set1.circleColors = [.darkGray]
         set1.drawVerticalHighlightIndicatorEnabled = false
         set1.drawHorizontalHighlightIndicatorEnabled = false
-        
+        set1.valueFont = UIFont.systemFont(ofSize: 14, weight: .semibold)
         let data = LineChartData(dataSet: set1)
         
         chartView.data = data
-    }
-    
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 }
