e0d27122d9cdaba0502d5f4b2a8ee4446e39b723
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-10-28 16:32:46 -0300

    rollback spm charts version, layout adjustments

--- ExpensesOrganizer/Features/Dashboard/ViewControllers/GraphicsTableViewCell.swift
+++ ExpensesOrganizer/Features/Dashboard/ViewControllers/GraphicsTableViewCell.swift
@@ -8,31 +8,37 @@
 import Charts
 import UIKit
 
-class GraphicsTableViewCell: UITableViewCell {
+class GraphicsTableViewCell: UITableViewCell, ChartViewDelegate {
     
     @IBOutlet weak var chartView: LineChartView!
     
     var yValues: [ChartDataEntry] = []
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        chartView.delegate = self
+        
         chartView.backgroundColor = .systemGray5
         chartView.gridBackgroundColor = .systemGray5
+        
         chartView.rightAxis.enabled = false
         chartView.leftAxis.enabled = false
         chartView.rightAxis.enabled = false
         chartView.legend.enabled = false
         chartView.xAxis.labelPosition = .bottom
+        chartView.xAxis.axisLineColor = .clear
         chartView.xAxis.gridColor = .clear
         chartView.leftAxis.gridColor = .clear
         chartView.rightAxis.gridColor = .clear
         chartView.xAxis.labelFont = UIFont.systemFont(ofSize: 18, weight: .semibold)
         chartView.xAxis.avoidFirstLastClippingEnabled = true
-        
-        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
         chartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: months)
         chartView.xAxis.granularity = 1
+        
+        let marker = PillMarker(color: .white, font: UIFont.boldSystemFont(ofSize: 14), textColor: .white)
+        chartView.marker = marker
         
         // Mock
         for i in 6..<12 {
@@ -41,6 +47,18 @@ class GraphicsTableViewCell: UITableViewCell {
         
         setData()
     }
+    
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        print(entry)
+    }
+    
+    class MarkerView: UIView {
+        @IBOutlet var valueLabel: UILabel!
+        @IBOutlet var metricLabel: UILabel!
+        @IBOutlet var dateLabel: UILabel!
+    }
+    
+    let markerView = MarkerView()
     
     func setData() {
         let set1 = LineChartDataSet(entries: yValues, label: "Rendimento")
@@ -51,7 +69,7 @@ class GraphicsTableViewCell: UITableViewCell {
         set1.circleColors = [.darkGray]
         set1.drawVerticalHighlightIndicatorEnabled = false
         set1.drawHorizontalHighlightIndicatorEnabled = false
-        set1.valueFont = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        set1.drawValuesEnabled = false
         let data = LineChartData(dataSet: set1)
         
         chartView.data = data
