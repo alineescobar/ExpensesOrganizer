fbd2de4988d92085d460aadc3162950112ee270f
Diego Henrique Silva Oliveira <53708053+DiegoHSO@users.noreply.github.com>
2021-10-28 17:33:38 -0300

    balance values are now formatted and currency shows based on user's locale

--- ExpensesOrganizer/Features/Dashboard/ViewControllers/DashboardViewController.swift
+++ ExpensesOrganizer/Features/Dashboard/ViewControllers/DashboardViewController.swift
@@ -130,15 +130,12 @@ extension DashboardViewController: UITableViewDataSource {
                 return UITableViewCell()
             }
             cell.balanceDelegate = self
-            cell.currencyLabel.text = Locale.current.localizedCurrencySymbol(forCurrencyCode: Locale.current.currencyCode ?? "R$")
             cell.showGraphicsImage.image = isShowingGraphics ? UIImage(systemName: "chevron.up") : UIImage(systemName: "chevron.down")
             cell.hideBalanceButton.setImage(isShowingBalance ? UIImage(systemName: "eye") : UIImage(systemName: "eyebrow"), for: .normal)
-            cell.currencyLabel.alpha = isShowingBalance ? 1 : 0
-            cell.balanceRoundedLabel.alpha = isShowingBalance ? 1 : 0
-            cell.balanceDecimalLabel.alpha = isShowingBalance ? 1 : 0
+            cell.balanceLabel.alpha = isShowingBalance ? 1 : 0
             cell.balanceStackView.backgroundColor = isShowingBalance ? UIColor.clear : UIColor.lightGray
-            // TODO: Set balance value based on user's Locale
-            // balance = ...
+            let balance = 2234.5
+            cell.balanceLabel.attributedText = getFormattedBalance(balance: balance, smallTextSize: 13.6, type: .screen)
             return cell
             
         case .graphics:
--- ExpensesOrganizer/Shared/Extensions.swift
+++ ExpensesOrganizer/Shared/Extensions.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+enum UIType {
+    case card, screen
+}
+
 extension UIView {
     func makeCircle() {
         layer.cornerRadius = frame.size.width / 2
@@ -52,4 +56,63 @@ extension Locale {
 
         return Locale(identifier: identifier).currencySymbol
     }
+}
+
+extension String {
+
+    var length: Int {
+        return count
+    }
+
+    subscript (i: Int) -> String {
+        return self[i ..< i + 1]
+    }
+
+    func substring(fromIndex: Int) -> String {
+        return self[min(fromIndex, length) ..< length]
+    }
+
+    func substring(toIndex: Int) -> String {
+        return self[0 ..< max(0, toIndex)]
+    }
+
+    subscript (ran: Range<Int>) -> String {
+        let range = Range(uncheckedBounds: (lower: max(0, min(length, ran.lowerBound)),
+                                            upper: min(length, max(0, ran.upperBound))))
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return String(self[start ..< end])
+    }
+}
+
+func getFormattedBalance(balance: Double, smallTextSize: CGFloat, type: UIType) -> NSAttributedString {
+    let currency: String = Locale.current.localizedCurrencySymbol(forCurrencyCode: Locale.current.currencyCode ?? "R$") ?? ""
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .currency
+    
+    if var formattedBalance = formatter.string(from: balance as NSNumber) {
+        
+        if Character(formattedBalance[currency.count]).isNumber {
+            let index = formattedBalance.index(formattedBalance.startIndex, offsetBy: currency.count)
+            formattedBalance.insert(" ", at: index)
+        }
+        
+        if type == .screen {
+            let index = formattedBalance.index(formattedBalance.startIndex, offsetBy: formattedBalance.count - 2)
+            formattedBalance.insert(" ", at: index)
+        }
+        
+        let amountText = NSMutableAttributedString(string: formattedBalance)
+        
+        amountText.setAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: smallTextSize, weight: .light),
+                                      NSAttributedString.Key.foregroundColor: UIColor.black],
+                                 range: NSRange(location: 0, length: currency.count + 1))
+        
+        amountText.setAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: smallTextSize, weight: .light),
+                                      NSAttributedString.Key.foregroundColor: UIColor.black],
+                                 range: NSRange(location: formattedBalance.count - 3, length: 3))
+       return amountText
+    }
+    
+    return NSAttributedString()
 }
