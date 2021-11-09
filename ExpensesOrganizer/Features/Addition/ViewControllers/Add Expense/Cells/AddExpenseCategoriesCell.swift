95d950545740b4436c02948e0781f7b18fedd430
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-11-09 14:39:40 -0300

    save proto table

---
+++ ExpensesOrganizer/Features/Addition/ViewControllers/Add Expense/Cells/AddExpenseCategoriesCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddExpenseCategoriesCell.swift
+//  ExpensesOrganizer
+//
+//  Created by Anderson Sprenger on 06/11/21.
+//
+
+import UIKit
+
+class AddExpenseCategoriesCell: UITableViewCell {
+
+    @IBOutlet weak var circle1: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
