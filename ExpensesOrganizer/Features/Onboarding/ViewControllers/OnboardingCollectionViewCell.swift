ef9379ff185ce3e0dab4b53964de9271747da361
juAlberti <julia.maia@acad.pucrs.br>
2021-11-10 17:35:13 -0300

    adding the firsts screens of onborading, still missing autolayout

---
+++ ExpensesOrganizer/Features/Onboarding/ViewControllers/OnboardingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingCollectionViewCell.swift
+//  ExpensesOrganizer
+//
+//  Created by Julia Alberti Maia on 10/11/21.
+//
+
+import UIKit
+
+class OnboardingCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ ExpensesOrganizer/Features/Onboarding/ViewControllers/OnboardingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingViewController.swift
+//  ExpensesOrganizer
+//
+//  Created by Julia Alberti Maia on 10/11/21.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
