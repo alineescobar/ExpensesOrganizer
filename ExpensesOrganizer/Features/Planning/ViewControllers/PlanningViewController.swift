d6cab44c702e2a1a54d26b80b067f066eae9ca9a
Aline Escobar <21069293+alineescobar@users.noreply.github.com>
2021-11-10 13:37:43 -0300

    adding the planning grid

--- ExpensesOrganizer/Features/Planning/ViewControllers/PlanningViewController.swift
+++ ExpensesOrganizer/Features/Planning/ViewControllers/PlanningViewController.swift
@@ -7,22 +7,65 @@
 
 import UIKit
 
-class PlanningViewController: UIViewController {
-
+class PlanningViewController: UIViewController, UICollectionViewDelegate {
+    
+    @IBOutlet weak var planningCollectionView: UICollectionView!
+    private let roundButtonID: String = "RoundButtonCollectionViewCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+// MARK: Navigation Visuals
+        self.navigationItem.title = "Planejar"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "WorkSans-Bold", size: 20) as Any]
+        self.navigationController?.navigationBar.topItem?.title = " "
+        self.navigationController?.navigationBar.tintColor = .black
+        self.navigationController?.navigationBar.backIndicatorImage = UIImage(systemName: "chevron.left")?.withInsets(UIEdgeInsets(top: 0, left: 15, bottom: 3, right: 0))
+        self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(systemName: "chevron.left")?.withInsets(UIEdgeInsets(top: 0, left: 15, bottom: 3, right: 0))
+        
+// MARK: Collection View Settings
+        setUpCollection()
+    }
+    private func setUpCollection() {
+        planningCollectionView.register(UINib(nibName: roundButtonID, bundle: nil), forCellWithReuseIdentifier: roundButtonID)
+        planningCollectionView.delegate = self
+        planningCollectionView.dataSource = self
+        
+        let layout = UICollectionViewFlowLayout()
+             layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 55
+        layout.minimumInteritemSpacing = 20
+        planningCollectionView.setCollectionViewLayout(layout, animated: true)
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension PlanningViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: roundButtonID, for: indexPath) as? RoundButtonCollectionViewCell
+        return cell ?? UICollectionViewCell()
     }
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+extension PlanningViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+            return UIEdgeInsets(top: 1.0, left: 40.0, bottom: 1.0, right: 40.0)
+        }
 
+        func collectionView(_ collectionView: UICollectionView,
+                       layout collectionViewLayout: UICollectionViewLayout,
+                       sizeForItemAt indexPath: IndexPath) -> CGSize {
+            //    swiftlint:disable force_cast
+            let lay = collectionViewLayout as! UICollectionViewFlowLayout
+            //    swiftlint:enable force_cast
+//            let widthPerItem = collectionView.frame.width / 4 - lay.minimumInteritemSpacing
+            
+            return CGSize(width: 56, height: 56)
+        }
 }
