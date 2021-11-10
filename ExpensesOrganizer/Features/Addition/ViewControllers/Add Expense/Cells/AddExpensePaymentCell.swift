9f5206a80deceb6ab08d2dd85ea89612f728e236
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-11-10 18:46:28 -0300

    addition payment & wallet localization

--- ExpensesOrganizer/Features/Addition/ViewControllers/Add Expense/Cells/AddExpensePaymentCell.swift
+++ ExpensesOrganizer/Features/Addition/ViewControllers/Add Expense/Cells/AddExpensePaymentCell.swift
@@ -9,4 +9,18 @@ import UIKit
 
 class AddExpensePaymentCell: UITableViewCell {
     static var identifier: String = "add-expense-payment-cell"
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentStack: UIStackView!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        titleLabel.text = NSLocalizedString("addition-expense-wallet", comment: "")
+        // selectionLabel.text = NSLocalizedString("", comment: "")
+        
+        contentStack.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        contentStack.isLayoutMarginsRelativeArrangement = true
+    }
 }
--- ExpensesOrganizer/Features/Addition/ViewControllers/Add Income/Cells/AddIncomeWalletCell.swift
+++ ExpensesOrganizer/Features/Addition/ViewControllers/Add Income/Cells/AddIncomeWalletCell.swift
@@ -9,4 +9,18 @@ import UIKit
 
 class AddIncomeWalletCell: UITableViewCell {
     static let identifier: String = "add-income-wallet-cell"
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentStack: UIStackView!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        titleLabel.text = NSLocalizedString("addition-income-wallet", comment: "")
+        // selectionLabel.text = NSLocalizedString("", comment: "")
+        
+        contentStack.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        contentStack.isLayoutMarginsRelativeArrangement = true
+    }
 }
