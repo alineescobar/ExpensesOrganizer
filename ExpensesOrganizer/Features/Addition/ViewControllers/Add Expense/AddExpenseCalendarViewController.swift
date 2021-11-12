9562f14c3531bc998ac83567e0a2a2401bfb0db1
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-11-11 21:50:19 -0300

    minor layout fixes

--- ExpensesOrganizer/Features/Addition/ViewControllers/Add Expense/AddExpenseCalendarViewController.swift
+++ ExpensesOrganizer/Features/Addition/ViewControllers/Add Expense/AddExpenseCalendarViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 class AddExpenseCalendarViewController: UIViewController {
     
     @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var containerView: UIView!
     weak var calendarDelegate: CalendarDelegate?
     var selectedDate: Date = Date()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         datePicker.setDate(selectedDate, animated: true)
+        containerView.layer.cornerRadius = 13
     }
     
     override func viewWillDisappear(_ animated: Bool) {
--- ExpensesOrganizer/Features/Addition/ViewControllers/Add Income/AddIncomeCalendarViewController.swift
+++ ExpensesOrganizer/Features/Addition/ViewControllers/Add Income/AddIncomeCalendarViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 class AddIncomeCalendarViewController: UIViewController {
     
     @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var containerView: UIView!
     weak var calendarDelegate: CalendarDelegate?
     var selectedDate: Date = Date()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         datePicker.setDate(selectedDate, animated: true)
+        datePicker.setValue(UIColor.white, forKeyPath: "textColor")
+        containerView.layer.cornerRadius = 13
     }
     
     override func viewWillDisappear(_ animated: Bool) {
